81ec81f88ee506c0d19faa6bc4a8f93e0d73b78b
unknown <vasanthgamer2000@gmail.com>
2023-09-14 11:44:57 +0530

    Changes have been made

---
+++ Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppUsers.Models;
+
+namespace WebAppUsers.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<User> users = new List<User>()
+        {
+            new User(){UserId =  1, UserName = "Sam", UserPwd = "sam@123", UserEmail = "sam@yahoo.com"},
+            new User(){UserId =  2, UserName = "Amit", UserPwd = "Amit@123", UserEmail = "Amit@yahoo.com"},
+            new User(){UserId =  3, UserName = "Kumar", UserPwd = "Kumar@123", UserEmail = "Kumar@yahoo.com"},
+            new User(){UserId =  4, UserName = "Anisha", UserPwd = "Anisha@123", UserEmail = "Anisha@yahoo.com"},
+            new User(){UserId =  5, UserName = "Vidisha", UserPwd = "Vidisha@123", UserEmail = "Vidisha@yahoo.com"},
+        };
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        public IActionResult Create(User model)
+        {
+            if(ModelState.IsValid)
+            {
+                users.Add(model);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+
+            
+        }
+    }
+}
